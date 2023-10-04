88f54cf7f36dba718dd7a5d43f778ffaace877e9
rajat <22002447+agentrajat@users.noreply.github.com>
2023-10-04 20:04:14 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,31 @@
+// create web server with nodejs
+// run: node comments.js
+// run: http://localhost:3000
+
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res) {
+    console.log('request was made: ' + req.url);
+    if (req.url === '/home' || req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/index.html').pipe(res);
+    } else if (req.url === '/contact') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/contact.html').pipe(res);
+    } else if (req.url === '/api/comments') {
+        var comments = [
+            {name: 'John', age: 30},
+            {name: 'Jane', age: 20},
+            {name: 'Sally', age: 40}
+        ];
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify(comments));
+    } else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        fs.createReadStream(__dirname + '/404.html').pipe(res);
+    }
+});
+
+server.listen(3000, 'localhost');
+console.log('Now listening to port 3000');
